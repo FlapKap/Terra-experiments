204df4486d5a13159d40e465609ac06122c3b17f
Kasper Hjort Berthelsen <khjo@itu.dk>
2024-04-30 23:55:18 +0200

    out stderr to file as well and append always

--- experiment_runner/resources/remote_bash.sh
+++ experiment_runner/resources/remote_bash.sh
@@ -6,6 +6,6 @@ readonly OUTFILE="${HOME}/.iot-lab/${EXP_ID}/serial_output"
 echo "Launch serial_aggregator with exp_id==${EXP_ID}" >&2
 while true
 do
-    serial_aggregator -i ${EXP_ID} 2> /dev/null 1> ${OUTFILE}
+    serial_aggregator -i ${EXP_ID} >> ${OUTFILE} 2>&1
     sleep 0.01
 done
