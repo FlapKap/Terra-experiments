2ede40e03ae31a86c1c24fcf5c5989735c06c706
kasper <khjo@itu.dk>
2024-08-28 17:21:21 +0000

    :))

---
+++ execute_exp.sh
@@ -0,0 +1,4 @@
+python3 -m experiment_runner ./experiment -c experiment_10_16byte_map.json > experiment_10_16byte_map.log;
+python3 -m experiment_runner ./experiment -c experiment_10_32byte_map.json > experiment_10_32byte_map.log;
+python3 -m experiment_runner ./experiment -c experiment_10_64byte_map.json > experiment_10_64byte_map.log;
+python3 -m experiment_runner ./experiment -c experiment_10_128byte_map.json > experiment_10_128byte_map.log;
